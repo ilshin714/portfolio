88a5f5ab04c86a5438cc6857f39709eaacc69dd0
Ilshin Ji <ilshin714@gmail.com>
2023-04-13 20:20:24 -0400

    Updates

--- src/data/aboutData.js
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Lisa (Ilshin). I'm a full stack developer based in Ontario, Canada.",
-    description2: "During the day I work on freelance projects for a local business or find projects to contribute my skills to make better world. I spent my leisure hours do some sports such as swimming or riding a bycicle. I love to learn and explore new arenas.",
-    image: 2
-}
